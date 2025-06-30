0251865e988ba746c0327e0872015f9f57bc9ae7
brummiLana <svetlana.brumm@student.htw-berlin.de>
2025-06-30 03:39:51 +0200

    Meine Beschreibung der Änderung

---
+++ src/main/java/de/htw_berlin/myprose/CollaborativeProse.java
@@ -0,0 +1,19 @@
+/*
+* SPDX-FileCopyrightText: 2025 Svetlana Brumm
+*
+* SPDX-License-Identifier: GPL-3.0
+*
+*/
+
+package de.htw_berlin.myprose;
+
+public class CollaborativeProse {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hallo! Das ist meine erste Komponente. Ich freue mich! Und du?");
+
+        
+    }
+}
